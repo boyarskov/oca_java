76c8b0af35394b25c5c23dfa29fc6c47ad707ff6
Artem Boyarskov <arboyarskov@ya.ru>
2022-02-27 13:05:54 +0300

    HomeWork lession 05

--- src/main/java/ru/oca/lession05/BankAccount.java
+++ src/main/java/ru/oca/lession05/BankAccount.java
@@ -2,14 +2,21 @@ package ru.oca.lession05;
 
 public class BankAccount {
     int balance;
+    String name;
 
     int popolnenieScheta(int plus) {
+        System.out.println(name + " Balance: " + balance);
+        System.out.println("Topping up the balance on " + plus);
         balance += plus;
+        System.out.println(name + " New balance: " + balance);
         return balance;
     }
 
     int snyatieSoScheta(int minus) {
+        System.out.println(name + " Balance: " + balance);
+        System.out.println("Reducing the balance on " + minus);
         balance -= minus;
+        System.out.println(name + " New balance: " + balance);
         return balance;
     }
 
@@ -20,10 +27,14 @@ public class BankAccount {
     public static void main(String[] args) {
         BankAccount account1 = new BankAccount();
         account1.balance = 55;
-        account1.showInfo();
-        account1.snyatieSoScheta(2);
-        account1.showInfo();
+        account1.name = "Alex";
         account1.popolnenieScheta(9);
-        account1.showInfo();
+        account1.snyatieSoScheta(12);
+        System.out.println();
+        BankAccount account2 = new BankAccount();
+        account2.balance = 1355;
+        account2.name = "Max";
+        account2.popolnenieScheta(231);
+        account2.snyatieSoScheta(1000);
     }
 }
