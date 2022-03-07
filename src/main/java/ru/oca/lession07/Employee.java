786ff5cdb40eafb8ca500c942be87c5333a74fa1
Artem Boyarskov <arboyarskov@ya.ru>
2022-03-07 14:15:04 +0300

    HomeWork lession 07

--- src/main/java/ru/oca/lession07/Employee.java
+++ src/main/java/ru/oca/lession07/Employee.java
@@ -4,49 +4,22 @@ public class Employee {
     int id;
     public String surname;
     int age;
-    private int salary;
+    private double salary;
     String department;
 
-    boolean Employee(int id, String surname, int age, int salary, String department) {
+    public Employee(int id) {
         this.id = id;
-        this.surname = surname;
-        this.age = age;
-        this.salary = salary;
-        this.department = department;
-        return true;
     }
 
-    public boolean Employee(int id, String surname, int salary) {
-        this.id = id;
+    Employee(String surname) {
         this.surname = surname;
-        this.salary = salary;
-        System.out.println("Salary: " + salary);
-        System.out.println("Surname: " + surname);
-        System.out.println("ID: " + id);
-        return true;
     }
 
-    private boolean Employee2(int id, String surname, int salary) {
-        this.id = id;
-        this.surname = surname;
+    private Employee (double salary) {
         this.salary = salary;
-        System.out.println("Salary: " + salary);
-        System.out.println("Surname: " + surname);
-        System.out.println("ID: " + id);
-        return true;
     }
 
-    boolean Employee3(int id, String surname, int salary) {
-        this.id = id;
-        this.surname = surname;
-        this.salary = salary;
-        System.out.println("Salary: " + salary);
-        System.out.println("Surname: " + surname);
-        System.out.println("ID: " + id);
-        return true;
-    }
-
-    public int getSalary() {
+    public double getSalary() {
         System.out.println("Salary: " + salary);
         return salary;
     }
@@ -59,12 +32,6 @@ public class Employee {
     public int getId() {
         System.out.println("ID: " + id);
         return id;
-    }
-
-    int salaryX2() {
-        salary *= 2;
-        System.out.println("New salary " + surname + ": " + salary);
-        return salary;
     }
 
 }
--- src/main/java/ru/oca/lession07/Modifier.java
+++ src/main/java/ru/oca/lession07/Modifier.java
@@ -2,8 +2,10 @@ package ru.oca.lession07;
 
 public class Modifier {
     public static void main(String[] args) {
-        Employee test = new Employee();
-        test.Employee(3, "Artem", 23322);
-        test.Employee3(3, "Artem", 23322);
+        Employee test = new Employee(5);
+        Employee test2 = new Employee("Ivanov");
+
+        System.out.println(test.id);
+        System.out.println(test2.surname);
     }
 }
