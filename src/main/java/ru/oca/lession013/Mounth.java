fcfeb08eb4e83ed0f359c4ef2fd6d2e9c7a2dfae
Artem Boyarskov <arboyarskov@ya.ru>
2022-03-27 18:16:10 +0300

    Homework lession 13 "Switch statement"

---
+++ src/main/java/ru/oca/lession013/Mounth.java
@@ -0,0 +1,48 @@
+package ru.oca.lession013;
+
+public class Mounth {
+    public Mounth(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("Jule");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Mounth mounth = new Mounth(3);
+    }
+}
