0fc4d818a86d2f501ed8660ff99089b01a95605a
Artem Boyarskov <arboyarskov@ya.ru>
2023-03-05 00:32:08 +0300

    Быстрая сортировка массива (Quick Sort или сортировка Хоара). Сложность алгоритма: O(n log n)

--- src/main/java/ru/oca/sort/MatrixBubbleSort.java
+++ src/main/java/ru/oca/sort/MatrixBubbleSort.java
@@ -1,16 +1,15 @@
 package ru.oca.sort;
 
-public class matrixBubbleSort {
+public class MatrixBubbleSort {
     public static int[][] matrixBubbleSort(int[][] sortMatrix){
-        int swap;
         for (int i = 0; i < sortMatrix.length; i++) {
             for (int j = 0; j < sortMatrix[i].length; j++) {
                 for (int k = 0; k < sortMatrix.length; k++) {
                     for (int l = 0; l < sortMatrix[k].length; l++) {
                         if (sortMatrix[i][j] <= sortMatrix[k][l]) {
-                            swap = sortMatrix[i][j];
-                            sortMatrix[i][j] = sortMatrix[k][l];
-                            sortMatrix[k][l] = swap;
+                           int swap = sortMatrix[i][j];
+                           sortMatrix[i][j] = sortMatrix[k][l];
+                           sortMatrix[k][l] = swap;
                         }
                     }
                 }
@@ -20,7 +19,7 @@ public class matrixBubbleSort {
     }
 
     public static void main(String args[]) {
-        int[][] sortMatrix = new int[][]{
+        int[][] sortMatrix = new int[][] {
                 {8, 3, 5},
                 {1, 4, 6},
                 {9, 7, 2}
@@ -29,7 +28,7 @@ public class matrixBubbleSort {
 
         for (int i = 0; i < sortMatrix.length; i++) {
             for (int j = 0; j < sortMatrix[i].length; j++) {
-                System.out.print(sortMatrix[i][j] + " ");
+                System.out.println(sortMatrix[i][j] + " ");
             }
             System.out.println();
         }
