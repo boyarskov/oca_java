28e8f1f93708a671305f6f6fdb62da521045d4b4
Artem Boyarskov <arboyarskov@ya.ru>
2023-06-27 15:10:07 +0300

    Аргументы командной строки

--- src/main/java/ru/oca/bookschildt/CommandLineArguments.java
+++ src/main/java/ru/oca/bookschildt/CommandLineArguments.java
@@ -8,14 +8,16 @@ public class CommandLineArguments {
                 {"а001аа52", "Mercedes-Benz S-class"},
                 {"о001оо52", "Лада Нива"}
         };
-        if (args.length != 1) {
-            System.out.println("Введите <номер> автомобиля");
+        if (args.length < 1) {
+            System.out.println("Введите <номер> или <номера> автомобилей");
         } else {
-            for (int i = 0; i < carsNumbers.length; i++) {
-                if (carsNumbers[i][0].equals(args[0])) {
-                    System.out.println("Госномер " + carsNumbers[i][0] + " установлен на: " + carsNumbers[i][1]);
-                    break;
+            for (int j = 0; j < args.length; j++) {
+                for (int i = 0; i < carsNumbers.length; i++) {
+                    if (carsNumbers[i][0].equals(args[j])) {
+                        System.out.println("Госномер " + carsNumbers[i][0] + " установлен на: " + carsNumbers[i][1]);
+                        break;
                 }
+            }
             }
         }
     }
