1c542387bad65e844c7a00421efb96b6203f1934
Artem Boyarskov <arboyarskov@ya.ru>
2023-11-12 00:04:03 +0300

    Restoring knowledge: for, loop

--- src/main/java/ru/oca/lession14/Oclock.java
+++ src/main/java/ru/oca/lession14/Oclock.java
@@ -1,23 +1,22 @@
 package ru.oca.lession14;
 
 public class Oclock {
-
-    public static void timer() {
+    static void timer() {
         OUTER: for(int i = 0; i < 6; i++) {
-            for (int j = 0; j < 60; j++) {
-                if (i > 1 && j % 10 == 0) {
+            MIDDLE: for (int j = 0; j < 60; j++) {
+                if (i > 1) {
                     break OUTER;
                 }
                 for (int k = 0; k < 60; k++) {
                     System.out.println(i + ":" + j + ":" + k);
                     if ((k * i) > j) {
-                        j++;
+                        continue MIDDLE;
                     }
                 }
             }
         }
     }
     public static void main(String[] args) {
-        Oclock.timer();
+        timer();
     }
 }
