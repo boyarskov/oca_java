8d410d733fc7300e5becd86878c1bba445f5cc0d
Artem Boyarskov <arboyarskov@ya.ru>
2024-01-19 12:52:26 +0300

    Restoring knowledge:StringBuilder

--- src/main/java/ru/oca/lession17/Sb.java
+++ src/main/java/ru/oca/lession17/Sb.java
@@ -4,18 +4,18 @@ public class Sb {
     public static void main(String[] args) {
         String name = new String("Fyodor");
         StringBuilder one = new StringBuilder();
-        StringBuilder two = new StringBuilder("Ivan");
-        StringBuilder three = new StringBuilder("Artyom Dostoevsky");
+        StringBuilder two = new StringBuilder("Artyom Dostoevsky");
+        StringBuilder three = new StringBuilder("Ани Лорак");
 
-        three.replace(0, 6, name);
-        System.out.println(three);
-        three.deleteCharAt(2);
-        System.out.println(three);
-        three.setCharAt(1, 'e');
-        System.out.println(three);
-        three.append(" написал книгу БЕСЫ");
-        System.out.println(three);
+        two.replace(0, 6, name)
+                .deleteCharAt(2)
+                .setCharAt(1, 'e');
+        two.append(" написал книгу БЕСЫ");
+        System.out.println(two);
+        String result = two.toString();
 
-        String result = three.toString();
+        three.deleteCharAt(3)
+                .reverse();
+        System.out.println(three);
         }
     }
