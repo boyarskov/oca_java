97ce43764a9130aab0737ddd6c3af67bff87bf10
Artem Boyarskov <arboyarskov@ya.ru>
2024-02-21 21:43:44 +0300

    Restoring knowledge:Switch Array

---
+++ src/main/java/ru/oca/lession18/SwitchArray.java
@@ -0,0 +1,26 @@
+package ru.oca.lession18;
+
+public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+        int temp = array[source];
+        array[source] = array[dest];
+        array[dest] = temp;
+        return array;
+    }
+
+
+    public static int[] swapBoarder(int[] array) {
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rsl = swapBoarder(numbers);
+        for (int i = 0; i > rsl.length; i++) {
+            System.out.println(rsl[i]);
+        }
+    }
+}
