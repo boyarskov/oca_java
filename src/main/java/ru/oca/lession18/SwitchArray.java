8057f2a9cf9eda72a8a7f9864e7786594d19b79c
Artem Boyarskov <arboyarskov@ya.ru>
2024-03-19 11:23:51 +0300

    Restoring knowledge: Rearrange the elements of the array

--- src/main/java/ru/oca/lession18/SwitchArray.java
+++ src/main/java/ru/oca/lession18/SwitchArray.java
@@ -8,7 +8,6 @@ public class SwitchArray {
         return array;
     }
 
-
     public static int[] swapBoarder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
@@ -19,8 +18,14 @@ public class SwitchArray {
     public static void main(String[] args) {
         int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
         int[] rsl = swapBoarder(numbers);
-        for (int i = 0; i > rsl.length; i++) {
+        for (int i = 0; i < rsl.length; i++) {
             System.out.println(rsl[i]);
+        }
+        System.out.println("stop swapBoarder");
+        int[] numbersSwap = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rslSwap = swap(numbersSwap, 3, 5);
+        for (int i = 0; i < rslSwap.length; i++) {
+            System.out.println(rslSwap[i]);
         }
     }
 }
